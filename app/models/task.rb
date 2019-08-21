a7939db5533d9ebef3b9e9a10a1227fe9a75fb5f
masako123 <h.masako123@gmail.com>
2019-08-21 14:08:55 +0900

    Create assigned_tasks method and add_assginee method

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,16 @@ class Task < ApplicationRecord
   belongs_to :assignee, :class_name => 'User'
   has_one_attached :image
   scope :order_by_created_at, -> {order(:created_at)}
+
+  before_validation :add_assignee, on: [ :create, :update ]
+
+  def add_assignee 
+      self.assignee_id = user_id unless assignee_id?
+  end
+
+  def done
+    binding.pry
+
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tasks
-  # has_many :assigned_tasks, :class_name => 'Task', :foreign_key => 'assigned_user_id'
+  has_many :assigned_tasks, :class_name => 'Task', :foreign_key => 'assignee_id'
+
+  def assigned_tasks
+    Task.where(assignee_id: id)
+  end
 
 end
